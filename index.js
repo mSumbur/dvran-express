36ad107ab518fb5b5c200ba63e2a9e29cdd82e0b
sumbur <m.sumbur@outlook.com>
2025-02-07 10:48:01 +0800

    test: test

--- index.js
+++ index.js
@@ -3,20 +3,24 @@ const fs = require('fs');
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
+
+// env
+const dotenv = require('dotenv')
+dotenv.config({
+  path: path.resolve(__dirname, './.env.' + process.env.NODE_ENV)
+})
+
 const { initDB } = require('./src/db')
-const { jwtAuth } = require("./src/middleware/jwtAuth");
-const { validate } = require("./src/middleware/validate");
-const { body, validationResult } = require('express-validator');
+const { jwtAuth } = require("./src/middleware/jwtAuth")
+
 // 错误处理插件
 require('express-async-errors');
-
-const logger = morgan("tiny")
 
 const app = express()
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 app.use(cors())
-app.use(logger)
+app.use(morgan("tiny"))
 app.use(jwtAuth)
 
 // 引入路由
@@ -38,7 +42,6 @@ app.use(function(err, req, res, next) {
   res.locals.code = err.status
   res.locals.message = err.message
   res.locals.error = req.app.get('env') === 'development' ? err : {}
-
   // render the error page
   res.status(err.status || 500)
   res.send(res.locals)
@@ -51,7 +54,7 @@ app.use(function(err, req, res, next) {
 //   }
 // })
 
-const port = process.env.PORT || 3003;
+const port = process.env.PORT || 3003
 
 async function bootstrap() {
   await initDB()
--- src/db/seq.js
+++ src/db/seq.js
@@ -2,7 +2,6 @@ const { Sequelize } = require("sequelize")
 
 // 从环境变量中读取数据库配置
 const { MYSQL_USERNAME, MYSQL_PASSWORD, MYSQL_ADDRESS = "" } = process.env
-
 const [host, port] = MYSQL_ADDRESS.split(":")
 
 const sequelize = new Sequelize("nodejs_demo", MYSQL_USERNAME, MYSQL_PASSWORD, {
